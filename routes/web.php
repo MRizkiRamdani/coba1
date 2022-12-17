cb8b2214ddd35263dd8d9e01d7abc3a01961d02b
MRizkiramdani <rizkyhero002@gmail.com>
2022-12-17 19:35:04 +0700

    bagian 5

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('admin/home', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.home')->middleware('is_admin');
+
+Route::get('admin/book', [App\Http\Controllers\AdminController::class, 'books'])->name('admin.books')->middleware('is_admin');
+
+Route::post('admin/books', [App\Http\Controllers\AdminController::class, 'submit_book'])->name('admin.book.submit')->middleware('is_admin');
+
+Route::patch('admin/books/update', [App\Http\Controllers\AdminController::class, 'update_book'])->name('admin.book.update')->middleware('is_admin');
+Route::get('admin/ajaxadmin/dataBuku/{id}', [App\Http\Controllers\AdminController::class, 'getDataBuku']);
+
+Route::post('admin/books/delete/{id}', [App\Http\Controllers\AdminController::class, 'delete_book'])->name('admin.book.delete')->middleware('is_admin');
+
+Route::get('admin/print_books', [App\Http\Controllers\AdminController::class, 'print_books'])->name('admin.print.books')->middleware('is_admin');
+
+Route::get('admin/books/export', [App\Http\controller\AdminController::class, 'export'])->name('admin.book.export')->middleware('is_admin');
+
+Route::post('admin/books/import', [App\Http\Controller\AdminController::class, 'import'])->name('admin.book.import')->middleware('is_admin');
